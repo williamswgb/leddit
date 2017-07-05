e270bab22bbc242aefd8661a9635305941128988
William Surya Wijaya <WilliamSW@Williams-MacBook-Pro.local>
2017-07-05 16:43:47 +0800

    Install and add redux-logger

--- src/store.js
+++ src/store.js
@@ -1,15 +1,19 @@
 import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
 import thunk from 'redux-thunk'
+import logger from 'redux-logger'
 
 import { reducer as Home } from 'pages/Home/reducer';
+import { reducer as Topic } from 'pages/Topic/reducer';
 
 const appReducer = combineReducers({
 	Home,
+	Topic,
 });
 
 export default createStore(
 	appReducer,
   applyMiddleware(thunk),
+	applyMiddleware(logger),
 	compose(
 		window.devToolsExtension ? window.devToolsExtension() : f => f
 	),
