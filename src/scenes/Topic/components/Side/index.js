9a9553d6dcfa190447138ef260f5e55629ff9f5d
William Surya Wijaya <WilliamSW@Williams-MacBook-Pro.local>
2017-07-06 15:06:34 +0800

    Add placeholder div for Side component

---
+++ src/scenes/Topic/components/Side/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+import './style.css'
+
+class SideContainer extends Component {
+  render() {
+    return (
+      <div className="Side">
+        Side
+      </div>
+    );
+  }
+}
+
+export default SideContainer
