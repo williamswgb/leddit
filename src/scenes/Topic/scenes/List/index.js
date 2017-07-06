9879c177af19d3c69757659988cd9e8061c8aed3
William Surya Wijaya <williamsw.gb@gmail.com>
2017-07-06 22:56:05 +0800

    Remove dummy data to redux

--- src/scenes/Topic/scenes/List/index.js
+++ src/scenes/Topic/scenes/List/index.js
@@ -1,31 +1,29 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { withRouter } from 'react-router-dom'
+import { array } from 'prop-types'
 
 import ListView from './listView.js'
 
 class ListContainer extends Component {
-  state = {
-    dummyTopics: Array(3).fill(
-      {
-        id: 1,
-        vote: 12,
-        title: 'Title',
-        description: 'Description',
-      }
-    )
+  static propTypes = {
+    data: array,
+  }
+
+  static defaultProps = {
+    data: []
   }
 
   render() {
     return (
-      <ListView data={this.state.dummyTopics} />
+      <ListView data={this.props.data} />
     )
   }
 }
 
 export default withRouter(connect((state) => {
-  const { Topic } = state
+  const { topics } = state.Topic.data;
   return {
-    topics: Topic.data.topics.byHash,
+    data: topics.byId.map((id) => topics.byHash[id]),
   }
 })(ListContainer))
