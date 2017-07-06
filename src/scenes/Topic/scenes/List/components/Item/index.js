af7b36c43cd0b066e89428054d4b69de3240cd2e
William Surya Wijaya <WilliamSW@Williams-MacBook-Pro.local>
2017-07-06 17:25:27 +0800

    Create Item component and separate into container and presentational

--- src/scenes/Topic/scenes/List/components/Item/index.js
+++ src/scenes/Topic/scenes/List/components/Item/index.js
@@ -1,15 +1,46 @@
 import React, { PureComponent } from 'react'
+import { object, number } from 'prop-types'
 
-import './style.css'
+import Item from './item.js'
 
-class Item extends PureComponent {
+class ItemContainer extends PureComponent {
+  static propTypes = {
+    data: object,
+    index: number,
+  }
+
+  static defaultProps = {
+    data: {},
+  }
+
+  state = {
+    isExpanded: this.props.index !== undefined,
+  }
+
+  handleClickExpand = this.props.index !== undefined ? false : () => {
+    this.setState({ isExpanded: !this.state.isExpanded })
+  }
+
+  handleClickExpand = (vote) => {
+    if (vote) {
+      // Upvote
+    } else {
+      // Downvote
+    }
+  }
+
   render() {
+    const { isExpanded } = this.state;
     return (
-      <div className="Item">
-        Item
-      </div>
+      <Item
+        {...this.props}
+        index={this.props.data.id}
+        isExpanded={isExpanded}
+        onClickExpand={this.handleClickExpand}
+        onClickVote={this.handleClickVote}
+      />
     );
   }
 }
 
-export default Item
+export default ItemContainer
