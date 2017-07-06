e4e1e004d4180aff294ca1439eb9484c69a480f1
William Surya Wijaya <williamsw.gb@gmail.com>
2017-07-06 21:37:27 +0800

    Update layout of item

--- src/scenes/Topic/components/Item/index.js
+++ src/scenes/Topic/components/Item/index.js
@@ -1,9 +1,12 @@
 import React, { PureComponent } from 'react'
 import { object, number } from 'prop-types'
 
-import ItemView from './itemView.js'
+import TextLink from 'components/TextLink'
+import Button from 'components/Button'
+import Vote from './components/Vote'
+import './style.css'
 
-class ItemContainer extends PureComponent {
+class Item extends PureComponent {
   static propTypes = {
     data: object,
     index: number,
@@ -14,33 +17,63 @@ class ItemContainer extends PureComponent {
   }
 
   state = {
-    isExpanded: this.props.index !== undefined,
+    isExpanded: this.props.index === undefined,
   }
 
-  handleClickExpand = this.props.index !== undefined ? false : () => {
+  handleClickExpand = this.props.index === undefined ? false : () => {
     this.setState({ isExpanded: !this.state.isExpanded })
   }
 
-  handleClickExpand = (vote) => {
-    if (vote) {
-      // Upvote
-    } else {
-      // Downvote
-    }
+  renderSubContent() {
+    const { data } = this.props
+    return (
+      <div className="Item-sub-content">
+        <div>{data.description}</div>
+      </div>
+    )
+  }
+
+  renderMainContent() {
+    const { data } = this.props
+    return (
+      <div className="Item-main-content">
+        <TextLink to={`/topic/${data.id}`}>
+          <div className="Item-title">{data.title}</div>
+        </TextLink>
+        <Button
+          className="Item-button"
+          onClick={this.handleClickExpand}
+          text={this.state.isExpanded ? 'Hide' : 'Show'}
+        />
+      </div>
+    )
+  }
+
+  renderIndex() {
+    const { index } = this.props
+
+    return index === undefined ? false :
+      <div className="Item-index">{index}</div>
+  }
+
+  renderContent() {
+    return (
+      <div className="Item-content-container">
+        {this.renderMainContent()}
+        {this.state.isExpanded ? this.renderSubContent() : false}
+      </div>
+    )
   }
 
   render() {
-    const { isExpanded } = this.state;
     return (
-      <ItemView
-        {...this.props}
-        index={this.props.data.id}
-        isExpanded={isExpanded}
-        onClickExpand={this.handleClickExpand}
-        onClickVote={this.handleClickVote}
-      />
+      <div className="Item">
+        {this.renderIndex()}
+        <Vote vote={this.props.data.vote} id={this.props.data.id} />
+        {this.renderContent()}
+      </div>
     );
   }
 }
 
-export default ItemContainer
+export default Item
