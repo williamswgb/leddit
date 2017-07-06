e06ca93408d3262347a26fa8a63ef9de21a2fe94
William Surya Wijaya <WilliamSW@Williams-MacBook-Pro.local>
2017-07-06 17:24:15 +0800

    Update List scenes component and style

--- src/scenes/Topic/scenes/List/index.js
+++ src/scenes/Topic/scenes/List/index.js
@@ -2,13 +2,27 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { withRouter } from 'react-router-dom'
 
+import TopicItem from './components/Item'
 import './style.css'
 
 class ListContainer extends Component {
+  state = {
+    dummyTopics: Array(3).fill(
+      {
+        id: 1,
+        vote: 12,
+        title: 'Title',
+        description: 'Description',
+      }
+    )
+  }
+
   render() {
     return (
       <div className="List">
-        List
+        {this.state.dummyTopics.map((topic, i) => (
+          <TopicItem key={`TopicItem-${i}`} data={topic} />
+        ))}
       </div>
     );
   }
