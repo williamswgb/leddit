a79c30ebb658bdb489ad7938140f4b2bcc29a386
William Surya Wijaya <WilliamSW@Williams-MacBook-Pro.local>
2017-07-06 15:11:12 +0800

    Add placeholder component for item

---
+++ src/scenes/Topic/scenes/List/components/Item/index.js
@@ -0,0 +1,15 @@
+import React, { PureComponent } from 'react'
+
+import './style.css'
+
+class Item extends PureComponent {
+  render() {
+    return (
+      <div className="Item">
+        Item
+      </div>
+    );
+  }
+}
+
+export default Item
