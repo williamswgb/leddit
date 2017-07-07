5405604686f45d2f467fb7b10926d686e8eec19c
William Surya Wijaya <williamsw.gb@gmail.com>
2017-07-07 22:28:53 +0800

    Add sorting function to data based on route

--- src/scenes/Topic/scenes/List/index.js
+++ src/scenes/Topic/scenes/List/index.js
@@ -1,22 +1,40 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { withRouter } from 'react-router-dom'
-import { array } from 'prop-types'
+import { array, object } from 'prop-types'
 
 import ListView from './listView.js'
 
 class ListContainer extends Component {
   static propTypes = {
     data: array,
+    match: object,
   }
 
   static defaultProps = {
-    data: []
+    data: [],
+    match: {},
+  }
+
+  sortData() {
+    const { data, match } = this.props
+    const route = (match.path || "").split('/')
+    switch(route[route.length - 1]) {
+      case 'hot':
+      case 'topic':
+        return data.sort((a,b) => a.vote < b.vote)
+      case 'new':
+        return data.sort((a,b) => a.id < b.id)
+      default:
+        return data
+    }
   }
 
   render() {
+    const data = this.props.data.length < 2 ?
+      this.props.data : this.sortData()
     return (
-      <ListView data={this.props.data} />
+      <ListView data={data} />
     )
   }
 }
