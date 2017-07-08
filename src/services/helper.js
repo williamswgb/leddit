bef8f82224f77cc61c7d315f521f4c5eaa4b7e87
William Surya Wijaya <williamsw.gb@gmail.com>
2017-07-08 17:56:17 +0800

    Add helper class to for helper functions

---
+++ src/services/helper.js
@@ -0,0 +1,20 @@
+class Helper {
+  static isEmptyObject(obj) {
+    return (this.isNullOrUndefined(obj)) ||
+           (Object.keys(obj).length === 0 && obj.constructor === Object);
+  }
+
+  static isNullOrUndefined(data) {
+    return typeof data === 'undefined' || data === null;
+  }
+
+  static isString(data) {
+    return typeof data === 'string';
+  }
+
+  static isNonEmptyString(data) {
+    return Helper.isString(data) && data.trim().length > 0;
+  }
+}
+
+export default Helper;
