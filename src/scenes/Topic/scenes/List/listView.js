3e07e4eaba17029a0446695c72a38b68a9be4917
William Surya Wijaya <williamsw.gb@gmail.com>
2017-07-08 22:14:52 +0800

    Add maxItems and page for index, update onClickPrev & onClickNext

--- src/scenes/Topic/scenes/List/listView.js
+++ src/scenes/Topic/scenes/List/listView.js
@@ -1,5 +1,5 @@
 import React, { PureComponent } from 'react'
-import { array, func } from 'prop-types'
+import { number, array, func } from 'prop-types'
 
 import Button from 'components/Button'
 import TopicItem from 'scenes/Topic/components/Item'
@@ -8,26 +8,31 @@ import './style.css'
 
 class ListView extends PureComponent {
   static propTypes = {
+    maxItems: number,
+    page: number,
     data: array,
     removeTopic: func,
-    goToNext: func,
-    goToPrev: func,
+    onClickNext: func,
+    onClickPrev: func,
   }
 
   static defaultProps = {
+    maxItems: null,
+    page: 1,
     data: [],
     removeTopic: null,
-    goToNext: null,
-    goToPrev: null,
+    onClickNext: null,
+    onClickPrev: null,
   }
 
   renderList() {
-    const { data, removeTopic } = this.props
+    const { data, removeTopic, page, maxItems } = this.props
 
     return data.map((topic, i) => (
       <TopicItem
         key={`TopicItem-${i}`}
-        data={topic} index={i+1}
+        data={topic}
+        index={((page - 1) * (maxItems || data.length))+i+1}
         basePath="/topic"
         onClickRemove={removeTopic}
       />
@@ -35,16 +40,15 @@ class ListView extends PureComponent {
   }
 
   renderNavigation() {
-    const { goToPrev, goToNext } = this.props
+    const { onClickPrev, onClickNext } = this.props
 
-    if (goToPrev || goToNext) {
+    if (onClickPrev || onClickNext) {
       return (
         <div className="List-navigation">
           <span className="List-navigation-text">view more:</span>
-          <Button text="< prev" onClick={goToPrev} />
-          {goToPrev && goToNext ? <div className="List-separator" />
-            : null}
-          <Button text="next >" onClick={goToNext} />
+          {onClickPrev ? <Button text="< prev" onClick={onClickPrev} /> : null}
+          {onClickPrev && onClickNext ? <div className="List-separator" /> : null}
+          {onClickNext ? <Button text="next >" onClick={onClickNext} /> : null}
         </div>
       )
     }
