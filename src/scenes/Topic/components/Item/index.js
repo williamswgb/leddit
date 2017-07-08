1ce3170915a7588e5f78f8b1e2efad8eef604561
William Surya Wijaya <williamsw.gb@gmail.com>
2017-07-08 11:35:45 +0800

    Add footer links and minor update component

--- src/scenes/Topic/components/Item/index.js
+++ src/scenes/Topic/components/Item/index.js
@@ -26,35 +26,67 @@ class Item extends PureComponent {
     this.setState({ isExpanded: !this.state.isExpanded })
   }
 
+  renderButtonContainer() {
+    const { data } = this.props
+
+    return (
+      <div className="Item-button-container">
+        {
+          this.props.index === null || data.text === undefined || data.text.trim() !== ''
+          ? null :
+          <Button
+            className="Item-button"
+            onClick={this.handleClickExpand}
+            text={this.state.isExpanded ? 'X' : '#'}
+          />
+        }
+        <div className="Item-time-submitted">
+          {`submitted ${moment(data.updatedAt).fromNow()}`}
+        </div>
+      </div>
+    )
+  }
+
+  renderFooter() {
+    const { data } = this.props
+    return (
+      <div className="Item-footer">
+        <TextLink to={`/topic/${data.id}/update`}>
+          <span className="Item-footer-link">update</span>
+        </TextLink>
+
+        <TextLink to={`/topic/${data.id}/remove`}>
+          <span className="Item-footer-link">remove</span>
+        </TextLink>
+      </div>
+    )
+  }
+
+  renderTitle() {
+    const { data } = this.props
+
+    return (
+      <TextLink to={`/topic/${data.id}`}>
+        <span className="Item-title">{data.title}</span>
+      </TextLink>
+    )
+  }
+
+  renderMainContent() {
+    return (
+      <div>
+        {this.renderTitle()}
+        {this.renderButtonContainer()}
+        {this.renderFooter()}
+      </div>
+    )
+  }
+
   renderSubContent() {
     const { data } = this.props
     return (
       <div className="Item-sub-content">
         <div>{data.text}</div>
-      </div>
-    )
-  }
-
-  renderMainContent() {
-    const { data } = this.props
-    return (
-      <div className="Item-main-content">
-        <TextLink to={`/topic/${data.id}`}>
-          <span className="Item-title">{data.title}</span>
-        </TextLink>
-        <div className="Item-button-container">
-          {
-            this.props.index === null ? null :
-            <Button
-              className="Item-button"
-              onClick={this.handleClickExpand}
-              text={this.state.isExpanded ? 'X' : '#'}
-            />
-          }
-          <div className="Item-time-submitted">
-            {`submitted ${moment(data.updatedAt).fromNow()}`}
-          </div>
-        </div>
       </div>
     )
   }
@@ -67,19 +99,22 @@ class Item extends PureComponent {
   }
 
   renderContent() {
+    const { data } = this.props
     return (
       <div className="Item-content-container">
         {this.renderMainContent()}
-        {this.state.isExpanded ? this.renderSubContent() : false}
+        {this.state.isExpanded && (data.text !== undefined && data.text.trim() !== '') ?
+          this.renderSubContent() : false}
       </div>
     )
   }
 
   render() {
+    const { data } = this.props
     return (
       <div className="Item">
         {this.renderIndex()}
-        <Vote vote={this.props.data.vote} id={this.props.data.id} />
+        <Vote vote={data.vote} id={data.id} />
         {this.renderContent()}
       </div>
     );
