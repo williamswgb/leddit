45f007a134e6145ec960ccd725fdb014d3a47909
William Surya Wijaya <williamsw.gb@gmail.com>
2017-07-08 19:32:00 +0800

    Update to pureComponent, add navigation buttons

--- src/scenes/Topic/scenes/List/listView.js
+++ src/scenes/Topic/scenes/List/listView.js
@@ -1,28 +1,63 @@
-import React from 'react'
+import React, { PureComponent } from 'react'
 import { array, func } from 'prop-types'
+
+import Button from 'components/Button'
 import TopicItem from 'scenes/Topic/components/Item'
 
-const ListView = ({ data, removeTopic }) => (
-  <div className="List">
-    {data.map((topic, i) => (
+import './style.css'
+
+class ListView extends PureComponent {
+  static propTypes = {
+    data: array,
+    removeTopic: func,
+    goToNext: func,
+    goToPrev: func,
+  }
+
+  static defaultProps = {
+    data: [],
+    removeTopic: null,
+    goToNext: null,
+    goToPrev: null,
+  }
+
+  renderList() {
+    const { data, removeTopic } = this.props
+
+    return data.map((topic, i) => (
       <TopicItem
         key={`TopicItem-${i}`}
         data={topic} index={i+1}
         basePath="/topic"
         onClickRemove={removeTopic}
       />
-    ))}
-  </div>
-)
+    ))
+  }
 
-ListView.propTypes = {
-  data: array,
-  removeTopic: func,
+  renderNavigation() {
+    const { goToPrev, goToNext } = this.props
+
+    if (goToPrev || goToNext) {
+      return (
+        <div className="List-navigation">
+          <span className="List-navigation-text">view more:</span>
+          <Button text="< prev" onClick={goToPrev} />
+          {goToPrev && goToNext ? <div className="List-separator" />
+            : null}
+          <Button text="next >" onClick={goToNext} />
+        </div>
+      )
+    }
+    return null
+  }
+
+  render() {
+    return (
+      <div className="List">
+        {this.renderList()}
+        {this.renderNavigation()}
+      </div>
+    )
+  }
 }
-
-ListView.defaultProps = {
-  data: [],
-  removeTopic: null,
-}
-
 export default ListView;
