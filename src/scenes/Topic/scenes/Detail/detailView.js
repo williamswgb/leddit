4a0b8a32847446d07bb5ff12556a55f634aacd4b
William Surya Wijaya <williamsw.gb@gmail.com>
2017-07-08 15:51:17 +0800

    Add basePath and onClickRemove props to item

--- src/scenes/Topic/scenes/Detail/detailView.js
+++ src/scenes/Topic/scenes/Detail/detailView.js
@@ -1,19 +1,27 @@
 import React from 'react'
-import { object } from 'prop-types'
+import { object, func, string } from 'prop-types'
 import TopicItem from 'scenes/Topic/components/Item'
 
-const DetailView = ({ data }) => (
+const DetailView = ({ data, basePath, onClickRemove }) => (
   <div className="Detail">
-    <TopicItem data={data} basePath="/topic" />
+    <TopicItem
+      data={data}
+      basePath={basePath}
+      onClickRemove={onClickRemove}
+    />
   </div>
 )
 
 DetailView.propTypes = {
   data: object,
+  basePath: string,
+  onClickRemove: func,
 }
 
 DetailView.defaultProps = {
   data: {},
+  basePath: '',
+  func: null,
 }
 
 export default DetailView;
