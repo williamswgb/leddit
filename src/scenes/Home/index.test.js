95aa67fe625be7a96e088740c9a8d5878a9a148a
William Surya Wijaya <williamsw.gb@gmail.com>
2017-07-09 17:09:16 +0800

    Add initial test

--- src/scenes/Home/index.test.js
+++ src/scenes/Home/index.test.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Home from './Home';
+import { shallow } from 'enzyme';
+import Home from './index';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<Home />, div);
+});
+
+it('renders shallowly without crashing', () => {
+  shallow(<Home />)
 });
