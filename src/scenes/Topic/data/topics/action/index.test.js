747ab0d45b363de1847c342a0b1bba8a9cbadca6
William Surya Wijaya <williamsw.gb@gmail.com>
2017-07-09 21:48:12 +0800

    Add test

--- src/scenes/Topic/data/topics/action/index.test.js
+++ src/scenes/Topic/data/topics/action/index.test.js
@@ -1,20 +1,171 @@
-// const showNotification = (id, text) => {
-//   return { type: 'SHOW_NOTIFICATION', id, text }
-// }
-//
-// const hideNotification = (id) => {
-//   return { type: 'HIDE_NOTIFICATION', id }
-// }
-//
-// let nextNotificationId = 0
-// const showNotificationWithTimeout = (text) => {
-//   return (dispatch) => {
-//     const id = nextNotificationId++
-//     dispatch(showNotification(id, text))
-//     setTimeout(() => {
-//       dispatch(hideNotification(id))
-//     }, 5000)
-//   }
-// }
-//
-// export { showNotificationWithTimeout }
+import configureMockStore from 'redux-mock-store'
+import thunk from 'redux-thunk'
+import moment from 'moment'
+
+import * as actions from './index'
+
+const middlewares = [thunk]
+const mockStore = configureMockStore(middlewares)
+
+const initialState = {
+  Topic: {
+    data: {
+      topics:{
+        byId: [],
+        order: [],
+        byHash: [],
+      }
+    }
+  }
+}
+
+describe('Topic actions', () => {
+  it('should create success actions when adding a valid topic', () => {
+    const payload = { title: 'Title test', text: 'Text test'}
+    const expectedActions = [
+      { type: actions.REQUEST },
+      { type: actions.CREATE, payload },
+      { type: actions.SUCCESS },
+    ]
+    const store = mockStore(initialState)
+    return store.dispatch(actions.create(payload)).then(() => {
+      expect(store.getActions()).toEqual(expectedActions)
+    })
+  })
+
+  it('should create error actions when adding an invalid topic with blank title', () => {
+    const payload = { title: '', text: ''}
+    const expectedActions = [
+      { type: actions.REQUEST },
+      { type: actions.ERROR, error: {"title": "Title can't be blank"} },
+    ]
+    const store = mockStore(initialState)
+    return store.dispatch(actions.create(payload)).catch(() => {
+      expect(store.getActions()).toEqual(expectedActions)
+    })
+  })
+
+  it('should create error actions when adding an invalid topic with title that exceed 256 characters', () => {
+    const payload = { title: Array(256).fill('a').join(''), text: ''}
+    const expectedActions = [
+      { type: actions.REQUEST },
+      { type: actions.ERROR, error: {"title": "Length of title can't exceed 255 characters"} },
+    ]
+    const store = mockStore(initialState)
+    return store.dispatch(actions.create(payload)).catch(() => {
+      expect(store.getActions()).toEqual(expectedActions)
+    })
+  })
+
+  it('should create success actions when updating with a valid topic', () => {
+    const id = 1
+    const payload = { title: 'Updated title test', text: 'Updated text test'}
+    const expectedActions = [
+      { type: actions.REQUEST },
+      { type: actions.UPDATE, payload, id },
+      { type: actions.SUCCESS },
+    ]
+    const store = mockStore(initialState)
+    return store.dispatch(actions.update(id, payload)).then(() => {
+      expect(store.getActions()).toEqual(expectedActions)
+    })
+  })
+
+  it('should create error actions when updating with an invalid topic with title that exceed 256 characters', () => {
+    const id = 1
+    const payload = { title: '', text: ''}
+    const expectedActions = [
+      { type: actions.REQUEST },
+      { type: actions.ERROR, error: {"title": "Title can't be blank"} },
+    ]
+    const store = mockStore(initialState)
+    return store.dispatch(actions.update(id, payload)).catch(() => {
+      expect(store.getActions()).toEqual(expectedActions)
+    })
+  })
+
+  it('should create error actions when updating with an invalid topic with title that exceed 256 characters', () => {
+    const id = 1
+    const payload = { title: Array(256).fill('a').join(''), text: ''}
+    const expectedActions = [
+      { type: actions.REQUEST },
+      { type: actions.ERROR, error: {"title": "Length of title can't exceed 255 characters"} },
+    ]
+    const store = mockStore(initialState)
+    return store.dispatch(actions.update(id, payload)).catch(() => {
+      expect(store.getActions()).toEqual(expectedActions)
+    })
+  })
+
+  it('should create error actions when removing a non-existing topic', () => {
+    const id = 1
+    const expectedActions = [
+      { type: actions.REQUEST },
+      { type: actions.REMOVE, id },
+      { type: actions.SUCCESS },
+    ]
+    const state = Object.assign({}, initialState)
+    state.Topic.data.topics = {
+      order: [],
+      byId: [1],
+      byHash: {
+        1: {
+          id: 1,
+          title: 'Title test',
+          text: 'Text test',
+          vote: 0,
+          updatedAt: moment().format(),
+        }
+      }
+    }
+    const store = mockStore(state)
+    return store.dispatch(actions.remove(id)).then(() => {
+      expect(store.getActions()).toEqual(expectedActions)
+    })
+  })
+
+  it('should create error actions when removing a non-existing topic', () => {
+    const id = 1
+    const expectedActions = [
+      { type: actions.REQUEST },
+      { type: actions.ERROR, error: {"id": "Topic not exist"} }
+    ]
+    const store = mockStore(initialState)
+    return store.dispatch(actions.remove(id)).catch(() => {
+      expect(store.getActions()).toEqual(expectedActions)
+    })
+  })
+
+  it('should create upvote topic action', () => {
+    const id = 1
+    const expectedActions = [
+      { type: actions.UPVOTE, id },
+    ]
+    const store = mockStore(initialState)
+
+    store.dispatch(actions.upvote(id))
+    expect(store.getActions()).toEqual(expectedActions)
+  })
+
+  it('should create downvote topic action', () => {
+    const id = 1
+    const expectedActions = [
+      { type: actions.DOWNVOTE, id },
+    ]
+    const store = mockStore(initialState)
+
+    store.dispatch(actions.downvote(id))
+    expect(store.getActions()).toEqual(expectedActions)
+  })
+
+  it('should create order topic action', () => {
+    const path = 'new'
+    const expectedActions = [
+      { type: actions.ORDER, order: [1] },
+    ]
+    const store = mockStore(initialState)
+
+    store.dispatch(actions.order(path))
+    expect(store.getActions()).toEqual(expectedActions)
+  })
+})
--- src/scenes/Topic/data/topics/reducer/index.test.js
+++ src/scenes/Topic/data/topics/reducer/index.test.js
@@ -1,18 +1,195 @@
-// import { combineReducers } from 'redux'
-//
-// const notifications = (state = [], action) => {
-//   switch (action.type) {
-//     case 'SHOW_NOTIFICATION':
-//       return [...state, { id: action.id, text: action.text }]
-//     case 'HIDE_NOTIFICATION':
-//       return state.filter((notification) => { return notification.id !== action.id })
-//     default:
-//       return state
-//   }
-// }
-//
-// const combinedReducers = combineReducers({
-//   notifications
-// })
-//
-// export default combinedReducers
+import moment from 'moment'
+import { reducer } from './index'
+import {
+  CREATE,
+  UPDATE,
+  REMOVE,
+  UPVOTE,
+  DOWNVOTE,
+  ORDER,
+} from '../action'
+
+const initialState = {
+  order: [],
+  byId: [],
+  byHash: {},
+}
+
+describe('Topics Reducer', () => {
+  it('should return the initial state', () => {
+    expect(reducer(undefined, {})).toEqual(initialState)
+  });
+
+  it('should handle CREATE', () => {
+    expect(
+      reducer(initialState, {
+        type: CREATE,
+        payload: {
+          title: 'Title test',
+          text: 'Text test',
+        },
+      })
+    ).toEqual({
+      order: [],
+      byId: [1],
+      byHash: {
+        1: {
+          id: 1,
+          title: 'Title test',
+          text: 'Text test',
+          vote: 0,
+          updatedAt: moment().format(),
+        }
+      }
+    })
+  })
+
+  it('should handle UPDATE', () => {
+    const state = {
+      order: [1],
+      byId: [1],
+      byHash: {
+        1: {
+          id: 1,
+          title: 'Title test',
+          text: 'Text test',
+          vote: 23,
+          updatedAt: "2017-07-09T20:13:06+08:00",
+        }
+      }
+    }
+
+    expect(
+      reducer(state, {
+        type: UPDATE,
+        id: 1,
+        payload: {
+          title: 'Updated title test',
+          text: 'Updated text test',
+        },
+      })
+    ).toEqual({
+      order: [1],
+      byId: [1],
+      byHash: {
+        1: {
+          id: 1,
+          title: 'Updated title test',
+          text: 'Updated text test',
+          vote: 23,
+          updatedAt: moment().format(),
+        }
+      }
+    })
+  })
+
+  it('should handle REMOVE', () => {
+    const state = {
+      order: [1],
+      byId: [1],
+      byHash: {
+        1: {
+          id: 1,
+          title: 'Title test',
+          text: 'Text test',
+          vote: 23,
+          updatedAt: "2017-07-09T20:13:06+08:00",
+        }
+      }
+    }
+
+    expect(
+      reducer(state, {
+        type: REMOVE,
+        id: 1
+      })
+    ).toEqual({
+      order: [],
+      byId: [],
+      byHash: {},
+    })
+  })
+
+  it('should handle UPVOTE', () => {
+    const state = {
+      order: [1],
+      byId: [1],
+      byHash: {
+        1: {
+          id: 1,
+          title: 'Title test',
+          text: 'Text test',
+          vote: 23,
+          updatedAt: "2017-07-09T20:13:06+08:00",
+        }
+      }
+    }
+
+    expect(
+      reducer(state, {
+        type: UPVOTE,
+        id: 1
+      })
+    ).toEqual({
+      order: [1],
+      byId: [1],
+      byHash: {
+        1: {
+          id: 1,
+          title: 'Title test',
+          text: 'Text test',
+          vote: 24,
+          updatedAt: "2017-07-09T20:13:06+08:00",
+        }
+      },
+    })
+  })
+
+  it('should handle DOWNVOTE', () => {
+    const state = {
+      order: [1],
+      byId: [1],
+      byHash: {
+        1: {
+          id: 1,
+          title: 'Title test',
+          text: 'Text test',
+          vote: 23,
+          updatedAt: "2017-07-09T20:13:06+08:00",
+        }
+      }
+    }
+
+    expect(
+      reducer(state, {
+        type: DOWNVOTE,
+        id: 1
+      })
+    ).toEqual({
+      order: [1],
+      byId: [1],
+      byHash: {
+        1: {
+          id: 1,
+          title: 'Title test',
+          text: 'Text test',
+          vote: 22,
+          updatedAt: "2017-07-09T20:13:06+08:00",
+        }
+      },
+    })
+  })
+
+  it('should handle ORDER', () => {
+    expect(
+      reducer(initialState, {
+        type: ORDER,
+        order: [5,1,3,4,2]
+      })
+    ).toEqual({
+      order: [5,1,3,4,2],
+      byId: [],
+      byHash: {},
+    })
+  })
+});
